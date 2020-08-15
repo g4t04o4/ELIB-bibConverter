0ddddd0759bb682fe69b53c160375aeb83f741e3
unknown <repmah@gmail.com>
2020-08-15 15:53:21 +0300

    New UI

---
+++ script.js
@@ -0,0 +1,4 @@
+var param = document.getElementById('param');
+var button = document.getElementById('button');
+var searchField = document.getElementById('query');
+var result = document.getElementById('result');
